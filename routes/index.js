fae6fe4f21933e04a086ad26c87ffe1b12246827
popoAOAOgroan <329565409@qq.com>
2016-11-18 16:20:12 +0800

    commit the new one :)

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,36 @@
 var express = require('express');
 var router = express.Router();
 var utilNext = require("../z_util/utilNext");
-var testControl = require("../z_controls/testControl");
+// var testControl = require("../z_controls/testControl");
+var assetsControl = require("../z_controls/assetsControl");
+
+
 /* GET home page. */
 router.get('/', function(req, res, next)
 {
   testControl.test(1,'2131',function(data)
   {
-    utilNext.utilRender(data,'index',res,next)
+    	utilNext.utilRender(data,'index',res,next)
   });
 });
 
 
-router.post('/',function(req,res,next)
-{
-  testControl.test(1,'2131',function(data)
-  {
-    utilNext.utilSend(data,res,next)
-  });
+// router.post('/',function(req,res,next)
+// {
+//   testControl.test(1,'2131',function(data)
+//   {
+//     	utilNext.utilSend(data,res,next)
+//   });
+// });
+
+router.get('/api/assets/all',function(req,res,next){
+	var assetsData = assetsControl.getAssetsList();
+	utilNext.utilSend(assetsData,res,next);
+});
+
+router.post('/api/assets/add',function(req,res,next){
+	var assetsData = assetsControl.addAssets(req);
+	utilNext.utilSend(assetsData,res,next);
 });
 
 module.exports = router;
---
+++ z_controls/assetsControl.js
@@ -0,0 +1,52 @@
+
+var assetsControl = module.exports;
+var netData = require("../z_models/netData");
+var code = require("../z_util/code");
+
+var mockData = {
+	results :[
+		{
+			name: '固定资产',
+			list: [
+				{
+					aName: '小财迷',
+					aTotal: 16023100,
+					aYesterdayCash: '+34.78',
+					aTotalRate: '+1.67%'
+				}
+			]
+		},
+		{
+			name: '浮动收益类',
+			list: [
+				{
+					aName: '千山药剂',
+					aTotal: 1265681,
+					aYesterdayCash: '-154.78',
+					aTotalRate: '+12.67%'
+				}
+			]
+		},
+		{
+			name: '其他资产',
+			list: [
+				{
+					aName: 'SurfacePro 4',
+					aTotal: 868800,
+					passDay: 9,
+					aYesterdayCash: '-0.00%'
+				}
+			]
+		}
+	]
+};
+
+assetsControl.getAssetsList = function(){
+    var returnData = new netData(code.success, mockData,'资产列表');
+    return returnData;
+}
+
+assetsControl.addAssets = function(){
+    var returnData = new netData(code.success, mockData,'增加资产');
+    return returnData;
+}
