32a522ead2376da53a31a81521caeb0d7232812f
abyy <arua@live.cn>
2016-12-01 17:13:50 +0800

    连接线上数据库

--- z_models/database.js
+++ z_models/database.js
@@ -1,11 +1,12 @@
 var mysql = require('mysql');
 
 var conn = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: 'nuoxiaobao',
-	database: 'test',
-	port: '3306'
+	host: 'qdm164036438.my3w.com',
+	user: 'qdm164036438',
+	password: 'wsBb0313',
+	database: 'qdm164036438_db',
+	port: '3306',
+	insecureAuth: true
 });
 conn.connect();
 
